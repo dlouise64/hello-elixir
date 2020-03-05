88dcd2b82346c1eb6878436268cb840ec2e7f3d3
dlouise64 <dana.iti.work@gmail.com>
2020-03-05 21:51:37 +1300

    some half pie docs

--- hello/mix.exs
+++ hello/mix.exs
@@ -42,7 +42,8 @@ defmodule Hello.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
     ]
   end
 
